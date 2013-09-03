52980deb8c1287f3561600de90ac9285c72e9a2a
hsgui <hsguidemail@gmail.com>
2013-09-03 14:30:46 +0800

    in order traversal and post traversal without recursion

--- LeetCode/src/tree/hsgui/com/BinarySearchTree.java
+++ LeetCode/src/tree/hsgui/com/BinarySearchTree.java
@@ -3,6 +3,8 @@ package tree.hsgui.com;
 import node.tree.hsgui.com.BSTNode;
 import util.hsgui.com.Util;
 
+import java.util.Stack;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hsgui
@@ -75,6 +77,81 @@ public class BinarySearchTree {
             }
         }else{
             return false;
+        }
+    }
+
+    public int maxTreeHeight(BSTNode root)
+    {
+        if (root == null) return 0;
+        return Math.max(maxTreeHeight(root.left), maxTreeHeight(root.right)) + 1;
+    }
+
+    /**
+     * cite: http://leetcode.com/2010/04/maximum-height-of-binary-tree.html
+     *       http://en.wikipedia.org/wiki/Tree_traversal#Post-order
+     * @param root
+     * @return the max height of the tree.
+     */
+    public int maxTreeHeightIterativeWithPostOrderTraversal(BSTNode root)
+    {
+        if (root == null) return 0;
+        Stack<BSTNode> stack = new Stack<BSTNode>();
+        int maxHeight = 0;
+        BSTNode previousNode = null;
+
+        stack.push(root);
+        while (!stack.isEmpty()){
+            BSTNode currentNode = stack.peek();
+            if (previousNode == null || previousNode.left == currentNode || previousNode.right == currentNode){
+                if (currentNode.left != null){
+                    stack.push(currentNode.left);
+                }else if (currentNode.right != null){
+                    stack.push(currentNode.right);
+                }
+            }else if (currentNode.left == previousNode){
+                if (currentNode.right != null){
+                    stack.push(currentNode.right);
+                }
+            }else{
+                //visit the currentNode;
+                stack.pop();
+            }
+            previousNode = currentNode;
+            if (stack.size() > maxHeight){
+                maxHeight = stack.size();
+            }
+        }
+        return maxHeight;
+    }
+
+    /**
+     * cite: http://leetcode.com/2010/04/binary-search-tree-in-order-traversal.html
+     * traversal the tree in order iteratively.
+     * 1. use the threaded binary tree to traversal the tree without recursion.
+     * 2. add a parent pointer to the data structure, this allows us to return to a node's parent.
+     *    To determine when to print a node's value, we would have to determine when it's returned from.
+     *    If it's returned from its left child, then print its value then traversa to its right child,
+     *    on the other hand if it's returned from its right child, then traversa up one level to its parent.
+     * @param root
+     */
+    public void inOrderTraversalIterative1(BSTNode root)
+    {
+        Stack<BSTNode> stack = new Stack<BSTNode>();
+        boolean done = false;
+        BSTNode current = root;
+        while (!done){
+            if (current != null){
+                stack.push(current);
+                current = current.left;
+            }else{
+                if (stack.empty()){
+                    done = true;
+                }else{
+                    current = stack.pop();
+                    System.out.println(current.value);
+                    current = current.right;
+                }
+            }
         }
     }
 
