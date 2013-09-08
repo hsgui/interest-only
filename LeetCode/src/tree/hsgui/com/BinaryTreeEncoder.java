1f77d5dae9227cf08e034228f348caa60b5e1c32
hsgui <hsguidemail@gmail.com>
2013-09-08 19:48:56 +0800

    encode the binary tree

---
+++ LeetCode/src/tree/hsgui/com/BinaryTreeEncoder.java
@@ -0,0 +1,29 @@
+package tree.hsgui.com;
+
+import node.tree.hsgui.com.BSTNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hsgui
+ * Date: 13-9-8
+ * Time: 下午1:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class BinaryTreeEncoder {
+
+    public static void encodeBinaryTreeToFile(BSTNode root, String fileName)
+    {
+        StringBuilder builder = new StringBuilder();
+
+    }
+
+    public static BSTNode decodeBinaryTreeFromFile(String fileName)
+    {
+        return null;
+    }
+
+    private static BSTNode decodeBinaryTreeFromFile()
+    {
+        return null;
+    }
+}
