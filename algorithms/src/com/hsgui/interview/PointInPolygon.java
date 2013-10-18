43a327b253253973179558f1811a67ae28310a43
hsgui <hsguidemail@gmail.com>
2013-10-18 23:00:10 +0800

    point in polygon using old-even rule

--- algorithms/src/com/hsgui/interview/PointInPolygon.java
+++ algorithms/src/com/hsgui/interview/PointInPolygon.java
@@ -19,6 +19,55 @@ package com.hsgui.interview;
  *    http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
  *
  * 3. http://en.wikipedia.org/wiki/Winding_number
+ *
+ * 4. http://en.wikipedia.org/wiki/Point_in_polygon
  */
 public class PointInPolygon {
+
+    public boolean isPointInPolygonUsingEvenOldRule(Point p, Point[] polygon){
+        boolean isInPolygonFlag = false;
+
+        for (int i = 0, j = polygon.length - 1; i < polygon.length; j = i, i++){
+            //p is the vertex of the polygon
+            if (p.equals(polygon[i]) || p.equals(polygon[j])){
+                isInPolygonFlag = true;
+                break;
+            }
+            if ((polygon[i].y < p.y && p.y <= polygon[j].y) ||
+                    (polygon[j].y < p.y && p.y <= polygon[i].y)){
+                double crossX = polygon[i].x + (double)(polygon[j].x - polygon[i].x)/(polygon[j].y - polygon[i].y)*(p.y - polygon[i].y);
+                //p is on the edge of the polygon.
+                if (crossX == p.x){
+                    isInPolygonFlag = true;
+                    break;
+                }else if (p.x > crossX){    //the ray cross the edge of Point[i]-->Point[j]
+                    isInPolygonFlag = !isInPolygonFlag;
+                }
+            }
+        }
+        return isInPolygonFlag;
+    }
+
+    public boolean isPointInPolygonUsingWindingNumber(Point p, Point[] polygon){
+        return false;
+    }
+}
+
+class Point{
+    public int x;
+    public int y;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point){
+            Point other = (Point)obj;
+            return other.x == x && other.y == y;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31*x + y;
+    }
 }
