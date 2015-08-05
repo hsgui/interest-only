27fd1472e4b7bf0b00e570b7b75f1cd6b69a8105
shuigui <hsguidemail@gmail.com>
2015-08-05 13:48:52 +0800

    add comments

--- LearningCPlusPlus/max.hpp
+++ LearningCPlusPlus/max.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
+// function template
+// instantiation: the process of replacing template parameters by concrete types
+//		It results in an instance of a template
+/*
+	templates are compiled twice:
+		1. without instantiation, the template code itself is checked for correct syntax
+		2. At the time of instantiation, the template code is checked to ensure that 
+			all calls in the template are valid
+
+	function template has two kinds of parameters:
+		1. template parameters
+		2. call parameters
+*/
 template<typename T>
 inline T const& max(T const& a, T const& b)
 {
