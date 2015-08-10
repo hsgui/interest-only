5dca11f533d9729e301e01bc821c6f134ba3984f
hsgui <hsguidemail@gmail.com>
2015-08-10 19:58:45 +0800

    lrvalue

--- LearningCPlusPlus/lrvalue.hpp
+++ LearningCPlusPlus/lrvalue.hpp
@@ -1,13 +1,23 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 
 namespace C11Practice
 {
 	/*
+		1. http://kuring.me/post/cpp11_right_reference
+		2. http://accu.org/index.php/journals/227
+		3. http://thbecker.net/articles/rvalue_references/section_02.html
+
 		lvalue: locator value, represents an object that occupies some identifiable location in memory
+				and allows us to take the address of that memory location via the & operator
+
+				think of expression as functions and then a lvalue can be thought as the result of a function returning a reference
+
 			i.e. has an address, a variable name
-		rvalue: not a lvalue, which doesn't represent an object that occupies some identifiable location in memory
+		rvalue: is an expression that is not an lvalue, which doesn't represent an object that occupies some identifiable location in memory
+				It's just a temporary value.
 			i.e. literal number, 1,
 	*/
 	class C11String
@@ -32,7 +42,7 @@ namespace C11Practice
 
 		/*
 			copy constructor
-				Lvalue reference
+			p_src is lvalue reference to C11String
 		*/
 		C11String(const C11String& p_src)
 		{
@@ -43,9 +53,15 @@ namespace C11Practice
 		}
 
 		/*
+			rvalue reference overload for copy constructor
 			move constructor:
 				the parameter is not const, and have &&
 				Rvalue reference
+
+			p_src is an rvalue reference to C11String
+
+			when it comes to function overload resolution
+			lvalues prefer old-style lvalue preferences, whereas rvalues prefer this new rvalue reference
 		*/
 		C11String(C11String&& p_src)
 		{
@@ -59,6 +75,27 @@ namespace C11Practice
 			p_src.m_length = 0;
 		}
 
+		/*
+			the lvalue reference can only be bound to lvalues but not rvalues
+			however, we could bind an rvalue to a const lvalue reference
+			e.g.	int& a = 3 error
+					const int& a = 3 ok
+
+			if we implement:
+				void foo(X&);
+				but not void foo(X&&);
+			then, foo can be called on lvalues, but not on rvalues, because rvalues can't be bound to non-const lvalue reference
+
+			if we implement:
+				void foo(X const&);
+				but not void foo(X&&);
+			then, foo can be called on lvalues and rvalues. But it's impossible to make it distinguish between lvalues and rvalues
+
+			if we implement:
+				void foo(X&&);
+				but neither one of void foo(X&) and void foo(X const&);
+			then, foo can only be called on rvalues, trying to call it on an lvalue will trigger a compile error
+		*/
 		C11String& operator=(const C11String& p_src)
 		{
 			if (this != &p_src)
@@ -71,12 +108,15 @@ namespace C11Practice
 				Init(p_src.m_data, p_src.m_length);
 			}
 
-			std::cout << "Copy Assignment is called! p_src->m_data=" << (long)p_src.m_data
+			std::cout << "Copy operator= is called! p_src->m_data=" << (long)p_src.m_data
 				<< "; this->m_data=" << (long)this->m_data << std::endl;
 
 			return *this;
 		}
 
+		/*
+			move semantics
+		*/
 		C11String& operator=(C11String&& p_src)
 		{
 			std::cout << "Move operator= is called! src: "
@@ -145,4 +185,124 @@ namespace C11Practice
 		}
 	};
 
+	template<typename T>
+	static void Swap(T& a, T&b)
+	{
+		/*
+			a, b are both lvalue reference
+
+			copy constructor and copy operator= are called, 
+			instead of move constructor and move operator=
+		*/
+		T temp(a);
+		a = b;
+		b = temp;
+	}
+
+	template<typename T>
+	static void SwapWithMove(T& a, T& b)
+	{
+		/*
+			a, b are both lvalue reference,
+			std::move will convert the parameter to rvalue without doing anything else
+			question:
+				how std::move implement?
+
+			http://thbecker.net/articles/rvalue_references/section_05.html
+
+		*/
+		T temp(std::move(a));
+		a = std::move(b);
+		b = std::move(temp);
+	}
+
+	/*
+		perfect forwarding
+		http://thbecker.net/articles/rvalue_references/section_07.html
+	*/
+	/*
+		the intent of factory1 function template is to forward the argument arg from the factory function to T's constructor.
+		Ideally, as far as arg is concerned, everything should behave just as if the factory function wern't there
+			and the constructor were called directly in the client code -- perfect forward
+	*/
+	/*
+		problem of factory1: 
+			it introduces an extra call by value (copty constructor) if the constructor of T takes its argument by reference
+				-- not perfect
+	*/
+	template<typename T, typename Arg>
+	static std::shared_ptr<T> factory1(Arg arg)
+	{
+		return std::shared_ptr<T>(new T(arg));
+	}
+
+	/*
+		problem of factory2:
+			the factory function can't be called on rvalues if T could construct by rvalue
+	*/
+	template<typename T, typename Arg>
+	static std::shared_ptr<T> factory2(Arg& arg)
+	{
+		return std::shared_ptr<T>(new T(arg));
+	}
+
+	/*
+		problem of factory3:
+			0. if T's constructor is T(Arg& );
+			1. if T's constructor takes serveral arguments, but not all argument are const,
+				e.g. T(Arg1 const&, Arg2 &);
+				then, all combinations of non-const and const reference for the various arguments
+			2. the argument of the constructor of T in the body of factory3 is a lvalue,
+				then, move semantics can never happen even if it would without wrapping function
+
+			-- not perfect
+	*/
+	template<typename T, typename Arg>
+	static std::shared_ptr<T> factory3(Arg const& arg)
+	{
+		return std::shared_ptr<T>(new T(arg));
+	}
+
+	/*
+		http://thbecker.net/articles/rvalue_references/section_08.html
+		reference collapsing rules:
+			A& & -> A&
+			A& && -> A&
+			A&& & -> A&
+			A&& && -> A&&
+
+		special template argument deduction rule for function templates that take an argument by rvalue reference to a template argument:
+			1. when factory is called on a lvalue of type A, then T resolves to A& and hence, by the reference collapsing rules,
+				 the argument type effectively becomes A&;
+			2. when factory is called on a rvalue of type A, then, T resolves to A, and hence, the argument type becomes A&&
+
+		note: typename std::remove_reference<S>::type is to declare std::remove_reference<S>::type is a type, 
+				otherwise, the compile will assume std::remove_reference<S>::type to be a value, which is the default behaviour
+			  remove_reference: http://en.cppreference.com/w/cpp/types/remove_reference
+
+			  possible implementation
+
+			  template<typename T> struct remove_reference		{typedef T type;};
+			  template<typename T> struct remove_reference<T&>	{typedef T type;};
+			  template<typename T> struct remove_reference<T&&> {typedef T type;};
+	*/
+	template<typename T>
+	typename std::remove_reference<T>::type&&
+		move(T&& a) noexcept
+	{
+		typedef typename std::remove_reference<T>::type&& RvalRef;
+		return static_cast<RvalRef>(a);
+	}
+
+	template<typename S>
+	S&& forward(typename std::remove_reference<S>::type& a) noexcept
+	{
+		return static_cast<S&&>(a);
+	}
+
+	template<typename T, typename Arg>
+	static std::shared_ptr<T> factory(Arg&& arg)
+	{
+		return std::shared_ptr<T>(new T(forward<Arg>(arg)));
+	}
 }
--- LearningCPlusPlus/lrvalueTest.hpp
+++ LearningCPlusPlus/lrvalueTest.hpp
@@ -4,10 +4,41 @@
 
 namespace C11Practice
 {
+	/*
+		http://thbecker.net/articles/rvalue_references/section_05.html
+		question:
+			rvalue reference is a rvalue?
+
+		p_src is a rvalue reference,
+		but in the line "C11String temp = p_src", copy constructor will be called
+
+		remember: if rvalue reference has a name, then it's a lvalue;
+				  if rvalue reference doesn't have a name, then it's a rvalue
+
+		Why is that?
+		we could think as following:
+			if the move constructor is called to construct temp object, then the data in p_src willn't be used!
+				what will it cause? 
+				then the following codes after the construction that use the p_src will lead to unexpected behavior
+	*/
+	void testRValueAndRValueReference(C11String&& p_src)
+	{
+		/* 
+			http://kuring.me/post/cpp11_right_reference
+		*/
+		std::cout << "temp object construct start" << std::endl;
+		C11String temp = p_src;		// note: copy constructor will be called, instead of move constructor!
+		// if we are quite sure p_src will not be used, we could use the following instead:
+		// C11String temp = std::move(p_src);
+		std::cout << "temp object construct finish: " << temp << std::endl;
+	}
+
 	C11String testC11StringValue()
 	{
 		C11String str("testStringValue");
 
+		// return std::move(str);	return value optimization
+		// http://thbecker.net/articles/rvalue_references/section_06.html
 		return str;
 	}
 	void testLRValue()
@@ -21,5 +52,22 @@ namespace C11Practice
 
 		std::cout << "IsLvalueReference(testC11StringValue()): "
 			<< C11String::IsLvalueReference(testC11StringValue()) << std::endl;
+
+		std::cout << "Start to Test the std::move() " << std::endl;
+		C11String a1("aaaa");
+		C11String b1("bbbb");
+		std::cout << "Before move: a=" << a1
+			<< "; b=" << b1 << std::endl;
+		C11Practice::Swap(a1, b1);
+		std::cout << "After swap is called: a=" << a1
+			<< "; b=" << b1 << std::endl;
+
+		C11Practice::SwapWithMove(a1, b1);
+		std::cout << "After SwapWithMove is called: a=" << a1
+			<< "; b=" << b1 << std::endl;
+
+		std::cout << "start to test rvalue and rvalue reference" << std::endl;
+		testRValueAndRValueReference(testC11StringValue());
+		std::cout << "finish to test rvalue and rvalue reference" << std::endl;
 	}
 }
