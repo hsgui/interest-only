1a84003b54dfdc198f02641beb83363cd02c2178
hsgui <hsguidemail@gmail.com>
2016-03-10 22:12:58 +0800

    update the repository

---
+++ leetcode/solutions/115-DistinctSubsequences.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+// https://leetcode.com/problems/distinct-subsequences/
+class DistinctSubsequences
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+
+    }
+};
---
+++ leetcode/solutions/334-IncreasingTripletSubsequence.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class IncreasingTripletSubsequence
+{
+public:
+    bool increasingTriplet(vector<int>& nums) 
+    {
+        return true;
+    }
+};
