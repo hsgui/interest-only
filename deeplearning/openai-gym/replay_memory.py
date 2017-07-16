252653a091e5cf93608cc9f8a7d231f98e668a10
Shuigui <hsguidemail@gmail.com>
2017-07-16 14:41:27 +0800

    Prioritized replay memory

--- deeplearning/openai-gym/replay_memory.py
+++ deeplearning/openai-gym/replay_memory.py
@@ -1,4 +1,5 @@
 import random
+from sumtree import SumTree
 
 class BasicReplayMemory:
     samples = []
@@ -18,3 +19,40 @@ class BasicReplayMemory:
 
     def isFull(self):
         return len(self.samples) >= self.capacity
+
+
+class PrioritizedReplayMemory:
+    e = 0.01
+    a = 0.6
+
+    def __init__(self, capacity):
+        self.tree = SumTree(capacity)
+
+    def _getPriority(self, error):
+        return (error + self.e) ** self.a
+
+    def add(self, error, sample):
+        p = self._getPriority(error)
+        self.tree.add(p, sample)
+
+    def sample(self, n):
+        batch = []
+        segment = self.tree.total() / n
+
+        for i in range(n):
+            a = segment * i
+            b = segment * (i+1)
+
+            s = random.uniform(a, b)
+            (idx, p, data) = self.tree.get(s)
+            batch.append( (idx, data) )
+
+        return batch
+
+    def update(self, idx, error):
+        p = self._getPriority(error)
+        self.tree.update(idx, p)
+
+    def isFull(self):
+        return self.tree.isFull()
+
